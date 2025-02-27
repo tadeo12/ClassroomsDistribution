0758fab00f4e41b82631272ee364f9923adf2827
Tadeo <tadeoova@gmail.com>
2025-02-27 09:33:19 -0300

    cambios

--- python/GUI/ResultsPage.py
+++ python/GUI/ResultsPage.py
@@ -5,8 +5,12 @@ def resultsPage():
     if "finalAllocation" in st.session_state and "evaluation" in st.session_state:
         st.subheader("Final Allocation")
 
-        final_allocation_serializable = {str(key): str(value) for key, value in st.session_state.finalAllocation.items()}
-        st.json(final_allocation_serializable)
+        detailedFinalAllocation = {str(key): str(value) for key, value in st.session_state.finalAllocation.items()}
+        finalAllocation = {key.id: value for key, value in st.session_state.finalAllocation.items() if value != -1}
+        if(st.radio("Mostrar asignación detallada", ("Sí", "No")) == "Sí"):
+            st.json(detailedFinalAllocation)
+        else:
+            st.json(finalAllocation)
         
         st.subheader("Evaluation")
         st.json(st.session_state.evaluation)
